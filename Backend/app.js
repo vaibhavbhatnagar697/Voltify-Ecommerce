c3108dde343a8b244d18bedb2f9c0a17bb76509b
vaibhavbhatnagar697 <vaibhavbhatnagar716@gmail.com>
2026-03-26 15:24:22 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true
+    origin: ["https://voltify-ecommerce-user.vercel.app", "http://localhost:5174"], credentials: true
 }))
 DbConnection()
 app.use(express.json({ limit: "50mb" }))
